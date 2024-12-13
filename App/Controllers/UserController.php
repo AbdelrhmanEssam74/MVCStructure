5562e91fcf53e6555e30da9dc67ed5d3ba0d9aa7
abdelrhman <abdelrhmanroshdy8@gmail.com>
2024-12-14 00:43:24 +0200

    Refactor `User` model and improve `profile` method in controller
    Removed unused signup logic and properties from the `User` model to simplify the class. Adjusted `profile` method in `UserController` for cleaner handling of user data by casting database results directly to an object.

--- App/Controllers/UserController.php
+++ App/Controllers/UserController.php
@@ -9,12 +9,12 @@ class UserController
 {
     public function profile($id = null)
     {// Fetch user data
-
         $userData = app()->db->row("SELECT * FROM `users` WHERE user_id = ?", [$id]);
+        $userData = (object) $userData[0];
         $date = [
             "userData" => [
-                'full_name' => $userData[0]->full_name,
-                'username' => $userData[0]->username,
+                'full_name' => $userData->full_name,
+                'username' => $userData->username,
             ]
         ];
         if ($date)
